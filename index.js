9a295f33d869f50ae39002d2b17667408cc0ee01
jeff <jeff@modulaweb.fr>
2022-07-11 15:55:37 +0200

    expose Intents

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const Command = require('./lib/Command.class');
 const SoundManager = require('./lib/SoundManager.class');
 const vocChanUtils = require('./lib/utils/vocChan');
 
+const { Intents } = require('discord.js');
+
+
 module.exports = {
   DiscordBot,
   Command,
   SoundManager,
   vocChanUtils,
+  Intents
 };
