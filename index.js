76ec70397be858a4e57c60fedc8cf8f0b7fc983c
jeff <jeff@modulaweb.fr>
2022-07-11 15:53:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const DiscordBot = require('./lib/DiscordBot.class');
+const Command = require('./lib/Command.class');
+const SoundManager = require('./lib/SoundManager.class');
+const vocChanUtils = require('./lib/utils/vocChan');
+
+module.exports = {
+  DiscordBot,
+  Command,
+  SoundManager,
+  vocChanUtils,
+};
